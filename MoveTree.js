303f97c7d040c6a38689422a6668078d2637bc99
Egle Libby <eglelibby@gmail.com>
2017-06-05 14:01:07 -0400

    building move tree

--- MoveTree.js
+++ MoveTree.js
@@ -1,20 +1,51 @@
-const Move = require('./Move');
+const Move = require("./Move");
 
-class MoveTree{
-    constructor(coordinates, maxDepth){
-        this.root = new Move(coordinates[0], coordinates[1], 0, [], null);
-        this.currentDepth = 0;
-        this.boardSize = 8;
+class MoveTree {
+  constructor(coordinates, maxDepth) {
+    this.root = new Move(coordinates[0], coordinates[1], 0, [], null);
+    this.maxDepth = maxDepth;
+    this.boardSize = 8;
+    this.generateMoves(this.root);
+  }
+
+  generateMoves(node, maxDepth) {
+    nextMoves(node);
+    currentDepth = 1;
+    while (maxDepth <= currentDepth) {
+      node.children.forEach(child => {
+        nextMoves(child);
+      });
+      node = child;
+      this.currentDepth++;
     }
-    
-    generateMoves(){
-        
+    function nextMoves(node) {
+      node.children.push(
+        new Move(node.x - 2, node.y - 1, node.depth + 1, [], node)
+      );
+      node.children.push(
+        new Move(node.x - 1, node.y - 2, node.depth + 1, [], node)
+      );
+      node.children.push(
+        new Move(node.x + 1, node.y - 2, node.depth + 1, [], node)
+      );
+      node.children.push(
+        new Move(node.x + 2, node.y - 1, node.depth + 1, [], node)
+      );
+      node.children.push(
+        new Move(node.x - 1, node.y + 2, node.depth + 1, [], node)
+      );
+      node.children.push(
+        new Move(node.x - 2, node.y + 1, node.depth + 1, [], node)
+      );
+      node.children.push(
+        new Move(node.x + 1, node.y + 2, node.depth + 1, [], node)
+      );
+      node.children.push(
+        new Move(node.x + 2, node.y + 1, node.depth + 1, [], node)
+      );
     }
-    
-    
+  }
 }
 
-
-
 //generate moves
-let tree = new MoveTree([3,3], 1);
+let tree = new MoveTree([3, 3], 1);
