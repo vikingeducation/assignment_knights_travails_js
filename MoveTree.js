458a163dc3e6b056b7d1b8a203d16e271a4235a8
Jerry Gao <gao@blackwright.com>
2017-06-05 14:04:28 -0400

    make function to check if move is legal and generate children for a starting node

--- MoveTree.js
+++ MoveTree.js
@@ -1,17 +1,42 @@
 class Move {
-  constructor(x, y, children, parent) {
+  constructor(x, y, depth, parent, children = []) {
     this.x = x;
     this.y = y;
-    this.children = children;
+    this.depth = depth;
     this.parent = parent;
+    this.children = children;
   }
 }
 
 class MoveTree {
-  constructor() {
-    this.visitedArray = new Array(8);
-    this.visitedArray.forEach(space => {
-      space = new Array(8);
-    });
+  constructor(start, maxDepth) {
+    this.root = new Move(start[0], start[1], 0);
+    // [ [], [], [], [], [], [], [], [] ]
+    this.visited = Array(8).fill().map(() => []);
+    this.visited[start[0]][start[1]] = true;
+    this.maxDepth = maxDepth;
+  }
+
+  legalMove(move) {
+    return !this.visited[move.x][move.y] &&
+           move.x <= 7 && move.x >= 0 &&
+           move.y <= 7 && move.y >= 0
+  }
+
+  possibleMoves(start) {
+    const moves = [];
+    [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]].forEach(move => {
+      const xMove = move[0];
+      const yMove = move[1];
+      const x = start.x + xMove;
+      const y = start.y + yMove;
+      if (legalMove({x, y}) && start.depth < this.maxDepth) {
+        moves.push(new Move(x, y, start.depth + 1, start));
+      }
+    })
+    return moves;
   }
 }
+
+const tree = new MoveTree();
+console.log(tree.visited);
