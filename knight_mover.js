21ec4686867064047ade430d77d4b660deddcf56
Alex-Willenbrink <awillenbrink@gmail.com>
2017-09-25 14:57:23 -0500

    nothing works right now

--- knight_mover.js
+++ knight_mover.js
@@ -10,42 +10,72 @@ class Move {
 class MoveTree {
   constructor(boardPos, maxDepth = 1) {
     this.maxDepth = maxDepth;
-    this.queue = [];
-    const possibleMoves = this.generateMoves(boardPos, boardPos);
-    possibleMoves.forEach(boardPos => {
-      this.queue.push(new Move(boardPos));
-    });
+    this.queue = [new Move(boardPos)];
+    for (let i = 0; i < 5; i++) {
+      this.generateNewDepth();
+    }
   }
 
-  generateMoves(boardPos, origin) {
+  generateNewDepth() {
+    const queueLength = this.queue.length;
+
+    for (let i = 0; i < queueLength; i++) {
+      this.generateNewMoves();
+    }
+  }
+
+  generateNewMoves() {
+    // we aren't adding the children to the parents right now
+    let possiblePositions = this.generateNewPositions(
+      this.queue[0],
+      knightMoves
+    );
+
+    possiblePositions.forEach(boardPos => {
+      this.queue.push(
+        new Move(boardPos, this.queue[0].depth + 1, [], this.queue[0])
+      );
+    });
+    console.log("this.queue: ", this.queue);
+    this.queue = this.queue.slice(1);
+  }
+
+  generateNewPositions(origin, moveArray) {
     const possibleMoves = [];
-    const knightMoves = [
-      [1, 2],
-      [2, 1],
-      [2, -1],
-      [1, -2],
-      [-1, 2],
-      [-2, -1],
-      [-1, -2],
-      [-2, 1]
-    ];
-    knightMoves.forEach(move => {
-      if (inBoard(7, 7, boardPos[0] + origin[0], boardPos[1] + origin[1]))
-        possibleMoves.push(move);
+
+    moveArray.forEach(move => {
+      const boardPos = [move[0] + origin[0], move[1] + origin[1]];
+      if (inBoard(boardPos, [7, 7])) possibleMoves.push(boardPos);
     });
     return possibleMoves;
   }
 
-  // nodes and
+  // nodes and depth
   inspect() {
     console.log("nodes: ", this.queue.length);
     console.log("maxDepth: ", this.maxDepth);
   }
 }
 
-const inBoard = (xBoard, yBoard, xMove, yMove) => {
-  return xMove >= 0 && xMove <= xBoard && yMove >= 0 && yMove <= yBoard;
+const inBoard = (boardPos, boardMax) => {
+  return (
+    boardPos[0] >= 0 &&
+    boardPos[0] <= boardMax[0] &&
+    boardPos[1] >= 0 &&
+    boardPos[1] <= boardMax[1]
+  );
 };
 
-const knightTree = new MoveTree([0, 0], 1);
+const knightMoves = [
+  [1, 2],
+  [2, 1],
+  [2, -1],
+  [1, -2],
+  [-1, 2],
+  [-2, -1],
+  [-1, -2],
+  [-2, 1]
+];
+
+const knightTree = new MoveTree([0, 0], 3);
 knightTree.inspect();
